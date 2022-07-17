5b29d135567d8d79370c5d234d7805c614ae3a8d
quang11103 <dominhquang11103@gmail.com>
2022-07-17 22:30:55 +0700

    ok

---
+++ resources/views/students/list-students.blade.php
@@ -0,0 +1,495 @@
+@extends("layout")
+@section("title")
+    <h1>List Students</h1>
+@endsection
+@section("main")
+    <!-- Content Header (Page header) -->
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">List Students</h3>
+
+            <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+                    <i class="fas fa-minus"></i></button>
+                <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
+                    <i class="fas fa-times"></i></button>
+            </div>
+        </div>
+        <div class="card-body p-0">
+            <table class="table table-striped projects">
+                <thead>
+                <tr>
+                    <th style="width: 1%">
+                        #
+                    </th>
+                    <th style="width: 10%">
+                        Student ID
+                    </th>
+                    <th style="width: 10%">
+                        Student Name
+                    </th>
+                    <th style="width: 10%">
+                        Student Birthday
+                    </th>
+                    <th style="width: 10%">
+                        Student Class
+                    </th>
+                    <th style="width: 10%">
+                        Images
+                    </th>
+
+                    <th style="width: 10%">
+
+                    </th>
+
+
+
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>
+                        #
+                    </td>
+                    <td>
+                        <a>
+                            AdminLTE v3
+                        </a>
+                        <br/>
+                    </td>
+                    <td>
+                        Name
+                    </td>
+                    <td>
+                        Unit
+                    </td>
+                    <td>
+                        Price
+                    </td>
+                    <td>
+                        <ul class="list-inline">
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar.png">
+                            </li>
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar2.png">
+                            </li>
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar3.png">
+                            </li>
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar04.png">
+                            </li>
+                        </ul>
+                    </td>
+
+
+
+                    <td class="project-actions text-right">
+                        <a class="btn btn-info btn-sm" href="#">
+                            <i class="fas fa-pencil-alt">
+                            </i>
+                            Edit
+                        </a>
+                        <a class="btn btn-danger btn-sm" href="#">
+                            <i class="fas fa-trash">
+                            </i>
+                            Delete
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        #
+                    </td>
+                    <td>
+                        <a>
+                            AdminLTE v3
+                        </a>
+                        <br/>
+
+                    </td>
+                    <td>
+                        Name
+                    </td>
+                    <td>
+                        Unit
+                    </td>
+                    <td>
+                        Price
+                    </td>
+                    <td>
+                        <ul class="list-inline">
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar.png">
+                            </li>
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar2.png">
+                            </li>
+                        </ul>
+                    </td>
+
+
+
+                    <td class="project-actions text-right">
+                        <a class="btn btn-info btn-sm" href="#">
+                            <i class="fas fa-pencil-alt">
+                            </i>
+                            Edit
+                        </a>
+                        <a class="btn btn-danger btn-sm" href="#">
+                            <i class="fas fa-trash">
+                            </i>
+                            Delete
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        #
+                    </td>
+                    <td>
+                        <a>
+                            AdminLTE v3
+                        </a>
+                        <br/>
+
+                    </td>
+                    <td>
+                        Name
+                    </td>
+                    <td>
+                        Unit
+                    </td>
+                    <td>
+                        Price
+                    </td>
+                    <td>
+                        <ul class="list-inline">
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar.png">
+                            </li>
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar2.png">
+                            </li>
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar3.png">
+                            </li>
+                        </ul>
+                    </td>
+
+
+
+                    <td class="project-actions text-right">
+                        <a class="btn btn-info btn-sm" href="#">
+                            <i class="fas fa-pencil-alt">
+                            </i>
+                            Edit
+                        </a>
+                        <a class="btn btn-danger btn-sm" href="#">
+                            <i class="fas fa-trash">
+                            </i>
+                            Delete
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        #
+                    </td>
+                    <td>
+                        <a>
+                            AdminLTE v3
+                        </a>
+                        <br/>
+
+                    </td>
+                    <td>
+                        Name
+                    </td>
+                    <td>
+                        Unit
+                    </td>
+                    <td>
+                        Price
+                    </td>
+                    <td>
+                        <ul class="list-inline">
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar.png">
+                            </li>
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar2.png">
+                            </li>
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar3.png">
+                            </li>
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar04.png">
+                            </li>
+                        </ul>
+                    </td>
+
+
+
+                    <td class="project-actions text-right">
+                        <a class="btn btn-info btn-sm" href="#">
+                            <i class="fas fa-pencil-alt">
+                            </i>
+                            Edit
+                        </a>
+                        <a class="btn btn-danger btn-sm" href="#">
+                            <i class="fas fa-trash">
+                            </i>
+                            Delete
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        #
+                    </td>
+                    <td>
+                        <a>
+                            AdminLTE v3
+                        </a>
+                        <br/>
+
+                    </td>
+                    <td>
+                        Name
+                    </td>
+                    <td>
+                        Unit
+                    </td>
+                    <td>
+                        Price
+                    </td>
+                    <td>
+                        <ul class="list-inline">
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar.png">
+                            </li>
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar04.png">
+                            </li>
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar5.png">
+                            </li>
+                        </ul>
+                    </td>
+
+
+
+                    <td class="project-actions text-right">
+                        <a class="btn btn-info btn-sm" href="#">
+                            <i class="fas fa-pencil-alt">
+                            </i>
+                            Edit
+                        </a>
+                        <a class="btn btn-danger btn-sm" href="#">
+                            <i class="fas fa-trash">
+                            </i>
+                            Delete
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        #
+                    </td>
+                    <td>
+                        <a>
+                            AdminLTE v3
+                        </a>
+                        <br/>
+
+                    </td>
+                    <td>
+                        Name
+                    </td>
+                    <td>
+                        Unit
+                    </td>
+                    <td>
+                        Price
+                    </td>
+                    <td>
+                        <ul class="list-inline">
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar.png">
+                            </li>
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar2.png">
+                            </li>
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar3.png">
+                            </li>
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar04.png">
+                            </li>
+                        </ul>
+                    </td>
+
+
+                    <td class="project-actions text-right">
+                        <a class="btn btn-info btn-sm" href="#">
+                            <i class="fas fa-pencil-alt">
+                            </i>
+                            Edit
+                        </a>
+                        <a class="btn btn-danger btn-sm" href="#">
+                            <i class="fas fa-trash">
+                            </i>
+                            Delete
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        #
+                    </td>
+                    <td>
+                        <a>
+                            AdminLTE v3
+                        </a>
+                        <br/>
+
+                    </td>
+                    <td>
+                        Name
+                    </td>
+                    <td>
+                        Unit
+                    </td>
+                    <td>
+                        Price
+                    </td>
+                    <td>
+                        <ul class="list-inline">
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar04.png">
+                            </li>
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar5.png">
+                            </li>
+                        </ul>
+                    </td>
+
+
+                    <td class="project-actions text-right">
+                        <a class="btn btn-info btn-sm" href="#">
+                            <i class="fas fa-pencil-alt">
+                            </i>
+                            Edit
+                        </a>
+                        <a class="btn btn-danger btn-sm" href="#">
+                            <i class="fas fa-trash">
+                            </i>
+                            Delete
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        #
+                    </td>
+                    <td>
+                        <a>
+                            AdminLTE v3
+                        </a>
+                        <br/>
+
+                    </td>
+                    <td>
+                        Name
+                    </td>
+                    <td>
+                        Unit
+                    </td>
+                    <td>
+                        Price
+                    </td>
+                    <td>
+                        <ul class="list-inline">
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar.png">
+                            </li>
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar3.png">
+                            </li>
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar04.png">
+                            </li>
+                        </ul>
+                    </td>
+
+
+                    <td class="project-actions text-right">
+                        <a class="btn btn-info btn-sm" href="#">
+                            <i class="fas fa-pencil-alt">
+                            </i>
+                            Edit
+                        </a>
+                        <a class="btn btn-danger btn-sm" href="#">
+                            <i class="fas fa-trash">
+                            </i>
+                            Delete
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        #
+                    </td>
+                    <td>
+                        <a>
+                            AdminLTE v3
+                        </a>
+                        <br/>
+                    </td>
+                    <td>
+                        Name
+                    </td>
+                    <td>
+                        Unit
+                    </td>
+                    <td>
+                        Price
+                    </td>
+                    <td>
+                        <ul class="list-inline">
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar.png">
+                            </li>
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar3.png">
+                            </li>
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar04.png">
+                            </li>
+                            <li class="list-inline-item">
+                                <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar5.png">
+                            </li>
+                        </ul>
+                    </td>
+
+                    <td class="project-actions text-right">
+                        <a class="btn btn-info btn-sm" href="#">
+                            <i class="fas fa-pencil-alt">
+                            </i>
+                            Edit
+                        </a>
+                        <a class="btn btn-danger btn-sm" href="#">
+                            <i class="fas fa-trash">
+                            </i>
+                            Delete
+                        </a>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <!-- /.card-body -->
+    </div>
+@endsection
